73c6ae8e05cd17a2fe6e37811e19225830b6f002
Similoluwa Adelore <62348760+AdeloreSimiloluwa@users.noreply.github.com>
2020-11-09 14:54:40 -0500

    Update doctype_processor.py

--- Kubecon/doctype_processor/src/doctype_processor.py
+++ Kubecon/doctype_processor/src/doctype_processor.py
@@ -3,7 +3,7 @@ import pandas as pd
 import argparse
 import logging  
 
-def doctype_processor() -> str:
+def produce_one_small_output() -> str:
   return "Hello Word"
 
 def print_small_text(text: str):
