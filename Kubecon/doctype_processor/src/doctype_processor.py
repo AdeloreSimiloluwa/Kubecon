e9af9368232e6e17e10e0dc0484c3d9a4dcf6a42
Similoluwa Adelore <62348760+AdeloreSimiloluwa@users.noreply.github.com>
2020-11-09 14:47:38 -0500

    Update doctype_processor.py

--- Kubecon/doctype_processor/src/doctype_processor.py
+++ Kubecon/doctype_processor/src/doctype_processor.py
@@ -3,28 +3,8 @@ import pandas as pd
 import argparse
 import logging  
 
-def doctype_processor(argv=None):
-  # Defining and parsing the command-line arguments
-  parser = argparse.ArgumentParser(description='My program description')
-  parser.add_argument('--outputdata', type=str,
-                      help='Path of the local file or GCS blob containing the Input 1 data.')
-  parser.add_argument('--vectorized', type=str, help='')
-  parser.add_argument('--image', type=str, help='')
-  parser.add_argument('--worddoc', type=str, help='')
-  parser.add_argument('--output-path-file', help='')
-  known_args, pipeline_args = parser.parse_known_args(argv)
-  args = parser.parse_args()
-  
-  a = known_args.vectorized
-  b = known_args.worddoc
-  c = known_args.image
-  
-  print(a,b,c,d)
-  
-  
-  Path(known_args.output_path_file).parent.mkdir(parents=True, exist_ok=True)
-  Path(known_args.output_path_file).write_text(known_args.outputdata) 
-  
+def doctype_processor() -> str:
+  return "Hello Word"
   
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
