3bfbcf3daf7d6e61c180fdd089d5a15cf0077140
Similoluwa Adelore <62348760+AdeloreSimiloluwa@users.noreply.github.com>
2020-11-09 12:01:17 -0500

    Update docloader.py

--- Kubecon/doc_loader/src/docloader.py
+++ Kubecon/doc_loader/src/docloader.py
@@ -7,10 +7,11 @@ from pathlib import Path
 
 # Defining and parsing the command-line arguments
 parser = argparse.ArgumentParser(description='My program description')
-parser.add_argument('--data',dest="data", type=str, required=False,
+parser.add_argument('--data', type=str ,  help= '',
                     help='Path of the local file or GCS blob containing the Input 1 data.')
 
-parser.add_argument('--outputdata',  dest="outputdata", type=str, required=False, default=argparse.SUPPRESS)
+parser.add_argument('--outputdata', type=str ,  help= '')
+parser.add_argument('--output-path-file', help='')
 args = parser.parse_args()
 print(os.path.dirname(args.outputdata))
 
@@ -21,3 +22,6 @@ data = pd.read_csv("https://raw.githubusercontent.com/AdeloreSimiloluwa/Kubecon/
 outputdata = pd.read_csv('https://raw.githubusercontent.com/AdeloreSimiloluwa/Kubecon/48f1681a89282870dfbdad926a3210877e22d1a8/Kubecon/data/outputdata.txt')
 logger = logging.getLogger('The data has successfully downloded')
 print(logger)
+
+Path(known_args.output_path_file).parent.mkdir(parents=True, exist_ok=True)
+Path(known_args.output_path_file).write_text(known_args.output) 
