8549c4be71cc2bb69ccdf23f125ac04736f222d6
Similoluwa Adelore <62348760+AdeloreSimiloluwa@users.noreply.github.com>
2020-11-09 09:07:57 -0500

    Update docloader.py

--- Kubecon/doc_loader/src/docloader.py
+++ Kubecon/doc_loader/src/docloader.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import argparse
-
+import logging  
 
 # Defining and parsing the command-line arguments
 parser = argparse.ArgumentParser(description='My program description')
@@ -13,5 +13,5 @@ args = parser.parse_args()
 
 data = pd.read_csv("https://raw.githubusercontent.com/AdeloreSimiloluwa/Kubecon/86a4b712940a8e1296fc7156b62c07cff3a2c0ee/Kubecon/data/data.txt")
 outputdata = pd.read_csv('https://raw.githubusercontent.com/AdeloreSimiloluwa/Kubecon/48f1681a89282870dfbdad926a3210877e22d1a8/Kubecon/data/outputdata.txt')
-logger = logger or logging.getLogger('The data has successfully downloded')
-print(logger)
+# logger = logging.getLogger('The data has successfully downloded')
+# print(logger)
